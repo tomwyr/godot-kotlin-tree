3905e93f285034bcbabb74c56a9da228321b95bb
tomwyr <tomwyr@hotmail.com>
2024-01-25 15:29:39 +0100

    Remove godot code from project root

--- example/src/main/kotlin/com/example/game/Simple.kt
+++ example/src/main/kotlin/com/example/game/Simple.kt
@@ -1,19 +1,6 @@
 package com.example.game
 
 import com.tomwyr.GodotNodeTree
-import godot.Node3D
-import godot.annotation.RegisterClass
-import godot.annotation.RegisterFunction
-import godot.global.GD
-
-@RegisterClass
-class Simple: Node3D() {
-
-    @RegisterFunction
-    override fun _ready() {
-        GD.print("Hello world!")
-    }
-}
 
 @GodotNodeTree
 object Test
