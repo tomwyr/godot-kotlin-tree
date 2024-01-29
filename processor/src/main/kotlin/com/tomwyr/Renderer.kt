48fe283b69b11d632c7c0f3734efb47832fd6c07
tomwyr <tomwyr@hotmail.com>
2024-01-29 09:28:39 +0100

    Generate simple node paths + make tree extension a function

--- processor/src/main/kotlin/com/tomwyr/Renderer.kt
+++ processor/src/main/kotlin/com/tomwyr/Renderer.kt
@@ -2,49 +2,73 @@ package com.tomwyr
 
 class NodeTreeRenderer {
     fun render(packageName: String, scenes: List<Scene>): String {
-        val extension = """
-        |val GD.Tree: GDTree
-        |    get() = nodeTree
+        val imports = """
+        |import godot.*
+        |import godot.core.NodePath
+        |import godot.global.GD
+        |import kotlin.reflect.KClass
+        """.trimMargin()
+
+        val treeExtension = """
+        |fun GD.Tree(node: Node): GDTree = nodeTree(node)
+        """.trimMargin()
+
+        val nodeExtension = """
+        |private inline fun <reified T : Node> Node.ref(path: String): T {
+        |    val node = getNode(NodePath(path)) ?: throw NodeNotFoundException(path)
+        |    (node as? T) ?: throw NodeInvalidTypeException(T::class)
+        |    return node
+        |}
+        |
+        |class NodeNotFoundException(path: String) : Exception("Node not found under given path ${'$'}path")
+        |
+        |class NodeInvalidTypeException(type: KClass<*>) : Exception("Node is not an instance of ${'$'}{type.simpleName}")
         """.trimMargin()
 
         val sceneObjects = scenes.joinToString("\n\n") { scene ->
-            val nodes = scene.nodes.joinToString("\n\t\t") { node ->
-                "override val ${node.name} = \"${node.type}\""
+            val nodes = scene.nodes.joinToString("\n") { node ->
+                """
+                |        override val ${node.name}: ${node.type} = node.ref("/root/${node.name}")
+                """.trimMargin()
             }
 
             """
             |    override val ${scene.name} = object : ${scene.name}Tree {
-            |        $nodes
+            |$nodes
             |    }
             """.trimMargin()
         }
         
         val rootObject = """
-        |private val nodeTree = object : GDTree {
+        |private fun nodeTree(node: Node): GDTree = object : GDTree {
         |$sceneObjects
         |}
         """.trimMargin()
 
         val rootInterface = run {
-            val nodes = scenes.joinToString("\n\t") { scene ->
-                "val ${scene.name}: ${scene.name}Tree"
+            val nodes = scenes.joinToString("\n") { scene ->
+                """
+                |    val ${scene.name}: ${scene.name}Tree
+                """.trimMargin()
             }
 
             """
             |interface GDTree {
-            |    $nodes
+            |$nodes
             |}
             """.trimMargin()
         }
 
         val sceneInterfaces = scenes.joinToString("\n\n") { scene ->
-            val nodes = scene.nodes.joinToString("\n\t") { node ->
-                "val ${node.name}: String"
+            val nodes = scene.nodes.joinToString("\n") { node ->
+                """
+                |    val ${node.name}: ${node.type}
+                """.trimIndent()
             }
 
             """
             |interface ${scene.name}Tree {
-            |    $nodes
+            |$nodes
             |}
             """.trimMargin()
         }
@@ -52,9 +76,11 @@ class NodeTreeRenderer {
         return """
         |package $packageName
         |
-        |import godot.global.GD
+        |$imports
         |
-        |$extension
+        |$treeExtension
+        |
+        |$nodeExtension
         |
         |$rootObject
         |
