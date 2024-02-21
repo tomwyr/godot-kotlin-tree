bf255a4dcac4ea17263f2ec76bf365d195bc5629
tomwyr <tomwyr@hotmail.com>
2024-02-21 23:37:52 +0100

    Add maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
+import java.io.FileInputStream
+import java.util.*
+
+version = "1.0.0-SNAPSHOT"
+group = "io.github.tomwyr"
+description = "A type-safe Godot node tree representation in Kotlin"
+
 plugins {
     id("com.gradle.plugin-publish") version "1.2.1"
     kotlin("jvm") version "1.9.0"
+    `maven-publish`
+    signing
 }
 
 repositories {
@@ -17,14 +26,78 @@ gradlePlugin {
     vcsUrl = "https://github.com/tomwyr/godot-kotlin-tree.git"
 
     plugins {
-        create("godot-node-tree") {
-            id = "io.github.tomwyr.godot-node-tree"
-            group = "io.github.tomwyr"
-            version = "1.0.0"
+        create("godot-kotlin-tree") {
+            id = "io.github.tomwyr.godot-kotlin-tree"
             displayName = "Godot Kotlin Tree"
-            description = "A type-safe Godot node tree representation in Kotlin."
             tags = listOf("godot", "kotlin", "node", "tree")
             implementationClass = "com.tomwyr.GodotNodeTree"
         }
     }
 }
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+val localProperties = Properties().apply {
+    load(FileInputStream(rootProject.file("local.properties")))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("plugin") {
+            artifactId = "godot-kotlin-tree"
+            from(components["java"])
+
+            pom {
+                name = "Godot Kotlin Tree"
+                description = "A type-safe Godot node tree representation in Kotlin"
+                url = "https://github.com/tomwyr/godot-kotlin-tree"
+
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://gist.github.com/tomwyr/2edea68a06b67d69b2a80964a3ff6f34"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "tomwyr"
+                        name = "Tomasz Wyrowiński"
+                        url = "https://github.com/tomwyr"
+                    }
+                }
+
+                scm {
+                    url = "https://github.com/tomwyr/godot-kotlin-tree"
+                    connection = "scm:git:git://github.com/tomwyr/godot-kotlin-tree.git"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/content/repositories/releases/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if (isSnapshot) snapshotsRepoUrl else releasesRepoUrl)
+
+            credentials {
+                username = localProperties["maven.repo.username"] as String
+                password = localProperties["maven.repo.password"] as String
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey = localProperties["maven.signing.key"] as String
+    val signingPassword = localProperties["maven.signing.password"] as String
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["plugin"])
+}
+
+val isSnapshot: Boolean
+    get() = version.toString().endsWith("SNAPSHOT")
