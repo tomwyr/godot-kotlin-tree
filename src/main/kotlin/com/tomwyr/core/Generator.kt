f4e50cf25c92f41b30aaecffb6c5a1f713ba018a
tomwyr <tomwyr@hotmail.com>
2024-05-16 18:58:03 +0200

    Restructure project for better test support

--- src/main/kotlin/com/tomwyr/core/Generator.kt
+++ src/main/kotlin/com/tomwyr/core/Generator.kt
@@ -1,38 +1,27 @@
 package com.tomwyr.core
 
-import java.io.File
-import java.io.OutputStream
-
 class NodeTreeGenerator(
-        private val parser: SceneNodesParser = SceneNodesParser(),
-        private val renderer: NodeTreeRenderer = NodeTreeRenderer(),
+    private val parser: SceneNodesParser = SceneNodesParser(),
+    private val renderer: NodeTreeRenderer = NodeTreeRenderer(),
 ) {
-    fun generate(projectPath: String, targetPackage: String?, createFile: () -> OutputStream): NodeTreeInfo {
+    fun generate(project: GodotKotlinProject): NodeTreeInfo {
         Log.readingScenes()
-        val scenes = readScenes(projectPath)
+        val scenesData = project.readScenes()
+        Log.scenesFound(scenesData)
+
+        val scenes = scenesData.map { data ->
+            Log.parsingScene(data)
+            val root = parser.parse(data.content)
+            Scene(data.name, root)
+        }.toList()
 
         Log.renderingNodeTree()
-        val content = renderer.render(targetPackage, scenes)
+        val content = renderer.render(project.targetPackage, scenes)
 
         Log.savingResult()
-        createFile().run {
-            write(content.toByteArray())
-            close()
-        }
+        project.writeNodeTree(content)
         Log.resultSaved()
 
         return NodeTreeInfo(scenes)
-    }
-
-    private fun readScenes(projectPath: String): List<Scene> {
-        val sceneFiles = File(projectPath).walkTopDown().filter { it.path.endsWith(".tscn") }.toList()
-        Log.scenesFound(sceneFiles)
-
-        return sceneFiles.map { file ->
-            Log.parsingScene(file)
-            val name = file.nameWithoutExtension
-            val root = parser.parse(file.readText())
-            Scene(name, root)
-        }
     }
 }
