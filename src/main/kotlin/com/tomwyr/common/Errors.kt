671f648e35b824fb305d5940f22895b4133f32c1
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-05-11 20:38:54 +0200

    Add temporary manual error coding

--- src/main/kotlin/com/tomwyr/common/Errors.kt
+++ src/main/kotlin/com/tomwyr/common/Errors.kt
@@ -1,6 +1,11 @@
+@file:OptIn(ExperimentalSerializationApi::class)
+
 package com.tomwyr.common
 
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonClassDiscriminator
 
 sealed class GodotKotlinTreeError : Exception() {
     override fun getLocalizedMessage(): String = when (this) {
@@ -11,6 +16,7 @@ sealed class GodotKotlinTreeError : Exception() {
 class GeneratorError(val error: GodotNodeTreeError) : GodotKotlinTreeError()
 
 @Serializable
+@JsonClassDiscriminator("errorType")
 sealed class GodotNodeTreeError : Exception() {
     override fun getLocalizedMessage(): String = when (this) {
         is InvalidGodotProject -> "Godot project could not be found at path `$projectPath`."
@@ -23,19 +29,25 @@ sealed class GodotNodeTreeError : Exception() {
 }
 
 @Serializable
+@SerialName("invalidGodotProject")
 class InvalidGodotProject(val projectPath: String) : GodotNodeTreeError()
 
 @Serializable
+@SerialName("scanningScenesFailed")
 class ScanningScenesFailed(val projectPath: String) : GodotNodeTreeError()
 
 @Serializable
+@SerialName("readingSceneFailed")
 class ReadingSceneFailed(val scenePath: String) : GodotNodeTreeError()
 
 @Serializable
+@SerialName("unexpectedNodeParameters")
 class UnexpectedNodeParameters(val nodeParams: NodeParams) : GodotNodeTreeError()
 
 @Serializable
+@SerialName("unexpectedSceneResource")
 class UnexpectedSceneResource(val instance: String) : GodotNodeTreeError()
 
 @Serializable
+@SerialName("parentNodeNotFound")
 class ParentNodeNotFound(val sceneName: String) : GodotNodeTreeError()
